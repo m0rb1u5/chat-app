5ec9d5167714c32e666cbecc9e1c9cfab7cb0190
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2024-07-04 12:37:34 -0300

    273. Adding Buttons & Models to the Authentication Screen

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -8,6 +8,8 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  bool _isLogin = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,6 +48,22 @@ class _AuthScreenState extends State<AuthScreen> {
                             ),
                             obscureText: true,
                           ),
+                          const SizedBox(height: 12),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+                            ),
+                            child: Text(_isLogin ? 'Login' : 'Signup'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(_isLogin ? 'Create an account' : 'I already have an account'),
+                          )
                         ],
                       ),
                     ),
